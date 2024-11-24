e4607d3552de58fb1131271fdbdb6f413ca280f5
Nahid Hasan <63138743+nahidhasan141400@users.noreply.github.com>
2024-11-24 22:42:58 +0600

    update GetPage.ts

--- src/controllers/front-end/GetPage.ts
+++ src/controllers/front-end/GetPage.ts
@@ -20,6 +20,24 @@ export const GetPageDataController = {
         // ... handle the condition
         const data = await db.AboutMinisterial.findAll();
         return res.send(data);
+      } else if (page === "about_cover") {
+        const data = await db.AboutCover.findOne();
+        return res.send(data);
+      } else if (page === "children_ministry") {
+        const data = await db.ChildrenMinistry.findOne();
+        return res.send(data);
+      } else if (page === "builder_ministry") {
+        const data = await db.BuilderMinistry.findOne();
+        return res.send(data);
+      } else if (page === "builder_ministry") {
+        const data = await db.ValourMinistry.findOne({
+          include: [
+            {
+              model: db.ValourC_Ministry,
+            },
+          ],
+        });
+        return res.send(data);
       }
     } catch (error) {
       next(error);
